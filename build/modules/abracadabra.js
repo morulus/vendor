df59b6972fea8ebfb50e29cba5c99c80a8227374
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-10-23 18:14:43 +0300

    hotfix

--- build/modules/abracadabra.js
+++ build/modules/abracadabra.js
@@ -1,4 +1,4 @@
-define(['wordBuilder', './firstUpper.js', './abra.js', './cada.js', './bra.js', './static.json', 'stylizer'], function(wordBuilder, firstUpper, abra, cada, bra, staticData) {
+define(['wordBuilder', './firstUpper.js', './abra.js', './cada.js', './bra.js', './static.json', './stylizer'], function(wordBuilder, firstUpper, abra, cada, bra, staticData) {
 	return function() {
 		return firstUpper(wordBuilder(abra, cada, bra, staticData.exclamation));
 	}
